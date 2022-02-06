675119d3d6dff519623c2d8c65d426c6b3de497a
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-02-06 15:35:08 +0300

    6-2-22 kullanıcı manager

--- BL/KullaniciManager.cs
+++ BL/KullaniciManager.cs
@@ -1,6 +1,8 @@
 ﻿using DAL;
+using Entities;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,28 @@ namespace BL
     public class KullaniciManager
     {
         DatabaseContext context = new DatabaseContext();
-
+        public List<Kullanici> GetAll()
+        {
+            return context.Kullanicilar.ToList();
+        }
+        public Kullanici Find(int id)
+        {
+            return context.Kullanicilar.Find(id); // find metodu geriye parametreden aldığı id ye sahip 
+        }
+        public int Add(Kullanici kullanici)
+        {
+            context.Kullanicilar.Add(kullanici);
+            return context.SaveChanges(); // geriye etkilenen kayıt sayısını döner
+        }
+        public int Update(Kullanici kullanici)
+        {
+            context.Kullanicilar.AddOrUpdate(kullanici);
+            return context.SaveChanges(); // geriye etkilenen kayıt sayısını döner
+        }
+        public int Delete(Kullanici kullanici)
+        {
+            context.Kullanicilar.Remove(kullanici);
+            return context.SaveChanges(); // geriye etkilenen kayıt sayısını döner
+        }
     }
 }
