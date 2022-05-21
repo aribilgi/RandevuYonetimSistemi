56b2dee4cdd9c52d33999b08da96db540a74d5d4
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-05-21 19:02:04 +0300

    21-05-22 MVC Katmanı oluşturuldu

--- RandevuYonetimSistemi.MvcUI/App_Start/BundleConfig.cs
+++ RandevuYonetimSistemi.MvcUI/App_Start/BundleConfig.cs
@@ -20,7 +20,7 @@ namespace RandevuYonetimSistemi.MvcUI
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
