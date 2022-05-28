50e66343a9de1bd6234790729b4009f33a0b1259
aribilgipendik@hotmail.com <aribilgipendik@hotmail.com>
2022-05-28 16:15:47 +0300

    28-5-22 Nihato

--- RandevuYonetimSistemi.MvcUI/Controllers/HomeController.cs
+++ RandevuYonetimSistemi.MvcUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BL;
+using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,20 @@ namespace RandevuYonetimSistemi.MvcUI.Controllers
 {
     public class HomeController : Controller
     {
+        ContactManager contactManager = new ContactManager();
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Ana Sayfa";
 
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ContactUs(Contact contact)
+        {
             return View();
         }
     }
